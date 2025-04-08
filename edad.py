e04a58e93208757c2dd3513ac440385741a64994
AlexMunoz9 <alex.2munoz9.@gmail.com>
2025-04-08 11:24:11 -0600

    ejercicios practicos

---
+++ edad.py
@@ -0,0 +1,12 @@
+
+a_nacimiento = int(input("Ingrese su año de nacimiento: "))
+a_actual = int(input("Ingrese el año actual: "))
+
+
+edad_actual = a_actual - a_nacimiento
+
+edad_en_10_anios = edad_actual + 10
+
+
+print(f"Su edad actual es: {edad_actual} años.")
+print(f"Su edad en 10 años será: {edad_en_10_anios} años.")
