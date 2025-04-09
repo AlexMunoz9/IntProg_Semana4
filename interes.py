d5c16414ac6ad4892816cfe78554e659a4bad129
AlexMunoz9 <alex.2munoz9.@gmail.com>
2025-04-09 00:18:55 -0600

    ejercicios practicos

---
+++ interes.py
@@ -0,0 +1,5 @@
+prestamo = 10000
+tasa = 27/100
+interes = prestamo * tasa
+
+print(f"El interés es: {interes:2f}")
---
+++ nomenclatura3.py
@@ -0,0 +1,11 @@
+#  Nombre completo del estudiante – camelCase
+#  Número de matrícula – snake_case
+#  Total de ventas del mes – PascalCase
+#  Límite máximo de intentos – SCREAMING_SNAKE_CASE
+#  Cantidad de productos – camelCase
+
+print("1. Nombre completo del estudiante – camelCase → nombreCompletoEstudiante")
+print("2. Número de matrícula – snake_case → numero_de_matricula")
+print("3. Total de ventas del mes – PascalCase → TotalDeVentasDelMes")
+print("4. Límite máximo de intentos – SCREAMING_SNAKE_CASE → LIMITE_MAXIMO_DE_INTENTOS")
+print("5. Cantidad de productos – camelCase → cantidadDeProductos")
